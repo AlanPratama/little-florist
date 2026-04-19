d92b993c688ffa42cc52c9998610a355e90add9b
AlanPratama <pralan76@gmail.com>
2026-04-19 12:00:22 +0700

    x

--- resources/views/pages/user/transaction/status/dikirim.blade.php
+++ resources/views/pages/user/transaction/status/dikirim.blade.php
@@ -69,7 +69,7 @@
                             <span class="font-semibold text-xl">Kode:</span> {{ $item->code }}
                         </div>
 
-                        @if (Auth::user()->role == 'Admin' || $item->user_id == Auth::user()->id)
+                        @if ($item->user_id == Auth::user()->id)
                             <div class="">
                                 <button type="button"
                                     class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-semibold rounded-lg text-sm px-3 py-2 text-center" data-modal-target="accept-modal" data-modal-toggle="accept-modal">Diterima</button>
